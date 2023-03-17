11299e408ef3011e33b4e01e6ac9937f6aeb524a
cuinixam <22344617+cuinixam@users.noreply.github.com>
2023-03-17 19:54:52 +0100

    feat: change docs to rst. use mermaid and autodoc

--- project/src/{{package_name}}/main.py
+++ project/src/{{package_name}}/main.py
@@ -1,2 +1,6 @@
-def add(n1: int, n2: int) -> int:
-    return n1 + n2
+class MyClass:
+    def __init__(self, counter: int):
+        self.counter = counter
+
+    def add(self, n1: int) -> int:
+        return n1 + self.counter
